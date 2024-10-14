9f08e96bc3f8a367ed7fc0d55dc96c885aa3ee62
Divyanxhucodes <divyanshuyadu@gmail.com>
2024-10-14 13:45:05 +0530

    Add files via upload

---
+++ to find factorial.cpp
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<conio.h>
+ int main()
+ {
+ 	int a,i,fact=1;
+ 	printf("enter the number you want to find factoial of : ");
+ 	scanf( "%d",&a);
+ 	for(i=1;i<=a;i++)
+ 	{
+	 fact=fact*i ;
+ 	}
+	 printf("%d factorial of number is :" , fact);
+ 	
+	 return 0;
+ }
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 
